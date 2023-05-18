899d9433f05d110c63ad45cbbdf89e2e6dcb2f40
MrShokhrukh <114140248+Mrshokhrukh@users.noreply.github.com>
2023-05-18 15:56:20 +0500

    lesson - 3 javascript

---
+++ 1-dars/3-dars.js
@@ -0,0 +1,68 @@
+// function myFunc(bir, ikki) {
+//   console.log(bir + ikki);
+// }
+
+// myFunc(12, 5);
+
+// function hisoblash() {
+//   a = "toshmat";
+//   console.log("bir");
+// }
+// console.log("ikki");
+// hisoblash();
+
+// console.log("uch");
+
+// function hisoblash() {
+//   let aQiymat = parseFloat(document.querySelector("#a").value);
+//   let bQiymat = parseFloat(document.querySelector("#a").value);
+
+//   document.querySelectorAll("p")[0].innerText = `${aQiymat} + ${bQiymat} = ${qoshish(
+//     aQiymat,
+//     bQiymat
+//   )}`;
+//   document.querySelectorAll("p")[1].innerText = `${aQiymat} - ${bQiymat} =  ${ayirish(
+//     aQiymat,
+//     bQiymat
+//   )}`;
+//   document.querySelectorAll("p")[2].innerText = `${aQiymat} / ${bQiymat} =  ${bolish(
+//     aQiymat,
+//     bQiymat
+//   )}`;
+//   document.querySelectorAll("p")[3].innerText = `${aQiymat} * ${bQiymat} =   ${kopaytirish(
+//     aQiymat,
+//     bQiymat
+//   )}`;
+// }
+
+// function qoshish(a, b) {
+//   let res = a + b;
+//   return res;
+// }
+
+// function ayirish(a, b) {
+//   let res = a - b;
+//   return res;
+// }
+
+// function bolish(a, b) {
+//   let res = a / b;
+//   return res;
+// }
+
+// function kopaytirish(a, b) {
+//   let res = a * b;
+//   return res;
+// }
+
+
+// -------------------------uyga vazifa-------------------------------
+
+// function gollarSoniniTop() {
+//   let uchlikGollar = parseInt(document.querySelector("#uch").value);
+//   let ikkilikGollar = parseInt(document.querySelector("#ikki").value);
+
+//   document.querySelector("h2").innerText = `yakuniy achko = ${
+//     3 * uchlikGollar + 2 * ikkilikGollar
+//   }`;
+// }
