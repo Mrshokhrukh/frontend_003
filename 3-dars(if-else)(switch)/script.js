03f504255a5ea781ea80f29f02cee848c14f8973
MrShokhrukh <114140248+Mrshokhrukh@users.noreply.github.com>
2023-05-23 15:56:39 +0500

    if else

---
+++ 3-dars(if-else)(switch)/script.js
@@ -0,0 +1,88 @@
+// 1) == ===
+// 2) var vs let
+// 3) null vs undefined
+// 4) undefined vs is not defined
+// 5) data types: Number, String, Boolean, Null, Undefined.
+// 6) const vs let
+// 7)
+
+
+
+// Shart Operatorlari 
+
+
+// console.log('bugun havo sovuq');
+// console.log('bugun havo issiq');
+// console.log('bugun havo bulut');
+// console.log("bugun yomg'ir");
+// console.log("bugun qor");
+
+
+// 0,undefined,false,null, === false
+// true,1 === true
+
+
+
+// let temp = "issiq";
+
+// if (temp === 'sovuq') {
+//     console.log('bugun havo sovuq');
+// } else if (temp === 'issiq') {
+//     console.log('bugun havo bulut');
+// } else if (temp === "yomg'ir") {
+//     console.log('bugun yomg\'ir');
+// } else if (temp === 'qor') {
+//     console.log('bugun qor');
+// }
+// else {
+//     console.log('bugun havo sovuq');
+// }
+
+
+
+// temp == yomg'ir 
+
+// 25 <= 40 issiq   25 >= 10 iliq   10 > 1 sovuq   1 < -20 qor
+
+// let temp = 12;
+
+// if (temp > 25 && temp < 40) {
+//     console.log('bugun issiq');
+// }
+// else if (temp < 25 && temp > 10) {
+//     console.log('bugun iliq');
+// } else if (temp < 10 && temp > 1) {
+//     console.log('bugun sovuq');
+// } else if (temp < 1 && temp > -20) {
+//     console.log('bugun qor');
+// }
+// else {
+//     console.log('bunaqa ob havo mavjud emas');
+// }
+
+
+
+
+
+
+// let temp = -10;
+
+// if (temp >= 30) {
+//     console.log('bugun issiq');
+// }
+// if (temp >= 20) {
+//     console.log('bugun iliq');
+// }
+// if (temp >= 10) {
+//     console.log('bugun sovuq');
+// }
+// if (temp >= 1) {
+//     console.log('bugun qor');
+// }
+// else {
+//     console.log('nth');
+// }
+
+
+// if(false){   }
+// else{  }
