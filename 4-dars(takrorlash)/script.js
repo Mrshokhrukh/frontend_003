2270d316de9a5e1773a241212b9a5ebba0db0b62
MrShokhrukh <114140248+Mrshokhrukh@users.noreply.github.com>
2023-05-27 15:44:40 +0500

    takrorlash

---
+++ 4-dars(takrorlash)/script.js
@@ -0,0 +1,18 @@
+
+// o'zgaruvchilar : let const, var || const va let, ES6
+
+
+// data types : number, string, boolean, null, undefined
+
+
+// operatorlar : ( +  *  -  / ) ++ -- +=  -= ( ===  ==  <=  >= )
+
+
+// function : chaqirilganda ishlaydi, hoisting, parametr, return
+
+
+// if else (ternary): shart operatori, if else, ternary =>  ?  :
+
+
+// switch case: switch case === 
+
