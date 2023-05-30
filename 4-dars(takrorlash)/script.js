d9da05352100424300ddfba6763be7e40a0adeba
MrShokhrukh <114140248+Mrshokhrukh@users.noreply.github.com>
2023-05-30 15:35:48 +0500

    5 dars

--- 4-dars(takrorlash)/script.js
+++ 4-dars(takrorlash)/script.js
@@ -16,3 +16,19 @@
 
 // switch case: switch case === 
 
+
+
+
+// function salom() {
+//     document.querySelector('h2').innerText += ', bugun 4 dars'
+// }
+
+// let toggle = false;
+// let getClass = document.querySelector('h2').classList;
+
+// function salom() {
+//     toggle ? toggle = false : toggle = true;
+
+//     toggle ? getClass.add('added') : getClass.remove('added')
+
+// }
---
+++ 5-dars/script.js
@@ -0,0 +1,70 @@
+//ichma ich funksiya
+
+// function funksiyaA() {
+
+//     function funksiyaB(a) {
+
+//     }
+
+
+// }
+
+// funksiyaA()
+
+
+
+
+//  -----------------------------Functions ==> function declaration va function expression ----------------------------;
+
+
+
+// function globalFunc(savol, isOk, isCancel) {
+//     if (confirm(savol)) isOk()
+//     else isCancel()
+// }
+
+// globalFunc('login qilganmi ?', funcOk, funckCancel)
+
+// function funcOk() {
+//     alert('siz login qilgansiz')
+// }
+// function funckCancel() {
+//     alert('siz hali login qilmagansiz')
+// }
+
+
+// function showText(a, b) {
+//     return a + b
+// }
+// let getFunc = showText(10, 20)
+
+
+// function expression
+
+
+
+
+// const ozgaruvchi = () => {
+//     return 'salom javascript ex ';
+// }
+
+// // =>  function expression hoistinga ega emas !
+
+// function dec() {
+//     return 'salom javascript dec';
+// }
+
+// // =>  function declaration hoistinga ega !
+
+
+// anonym funksiya deb nomsiz funcsiyaga aytiladi
+
+
+
+// const ozgaruvchi = () => {
+//     return 'salom javascript ex ';
+// }                  
+
+// ===>  arrow function;
+
+
