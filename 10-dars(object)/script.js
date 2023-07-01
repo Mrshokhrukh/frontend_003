a180b8202f86a860c0a35e9d4eda96f741b5a08b
MrShokhrukh <114140248+Mrshokhrukh@users.noreply.github.com>
2023-07-01 15:56:36 +0500

    object

---
+++ 10-dars(object)/script.js
@@ -0,0 +1,93 @@
+
+
+const obj = {
+    hp: '500',
+    name: 'lombarghini',
+    color: 'yellow',
+}
+
+const object = new Object()
+
+// object.name = 'jon',
+//     object.username = 'doe'
+// object.surname = '57'
+
+
+// for (let a in object) {
+//     console.log(object[a]);
+// }
+
+
+
+// let newObj = {
+//     firsName: 'Steve',
+//     lastName: 'johns',
+//     age: 32,
+//     fullName: function () {
+//         return this.firsName + ' ' + this.lastName
+//     }
+// }
+// let newObj2 = {
+//     firsName: 'Steve',
+//     lastName: 'jobs',
+//     age: 42,
+//     fullName: function () {
+//         return this.firsName + ' ' + this.lastName
+//     }
+// }
+// let newObj3 = {
+//     firsName: 'jonatan',
+//     lastName: 'swift',
+//     age: 72,
+//     fullName: function () {
+//         return this.firsName + ' ' + this.lastName
+//     }
+// }
+
+
+// console.log(newObj.fullName());
+// console.log(newObj2.fullName());
+// console.log(newObj3.fullName());
+
+
+
+
+// function Contructure(firsName, lastName, age) {
+//     this.firsName = firsName,
+//         this.lastName = lastName,
+//         this.age = age
+// }
+
+// let getJones = new Contructure('steve', 'johnes', 54)
+// let getJobs = new Contructure('steve', 'jobs', 44)
+// let getSwift = new Contructure('jonatan', 'swift', 73)
+
+
+
+// let newObj = {
+//     firsName: 'Steve',
+//     lastName: 'johns',
+//     age: 32,
+//     fullName: function () {
+//         return this.firsName + ' ' + this.lastName
+//     }
+// }
+
+// console.log(Object.keys(newObj));
+// console.log(Object.values(newObj));
+
+
+
+// let students = [
+//     { name: 'akobir', id: 3, sum: 150 },
+//     { name: 'sardor', id: 1, sum: 200 },
+//     { name: 'eshmat', id: 5, sum: 100 },
+//     { name: 'toshmat', id: 6, sum: 280 },
+//     { name: 'begzod', id: 4, sum: 300 },
+//     { name: 'diyor', id: 2, sum: 420 },
+// ]
+
+// let response = students.sort((a, b) => {
+//     return a.id - b.id
+// })
+
